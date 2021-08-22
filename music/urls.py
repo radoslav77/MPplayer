883bffee7e0335b82db748954ed2887624d5cab7
unknown <rado7711@yahoo.co.uk>
2021-08-22 18:45:14 +0100

    #0002 start of the functions

--- music/urls.py
+++ music/urls.py
@@ -1,5 +1,10 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
+
+app_name = "App"
 
 urlpatterns = [
     path('', views.index, name='index')
--- music/views.py
+++ music/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
+from .models import *
 
 
 def index(request):
-    pass
+    paginator = Paginator(Song.objects.all(), 1)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {"page_obj": page_obj}
+    return render(request, 'music/index.html', context)
+
+
+#form = Form(request.POST, request.FILES)
+
+
+# <form action="" method="post" enctype="multipart/form-data">
